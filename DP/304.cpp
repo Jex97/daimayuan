63b14d5b0258b44a242d5ef73cc484e16663e860
XJ-LapTop <746806139@qq.com>
2022-08-28 12:57:27 +0800

    main 函数的小修改

--- DP/304.cpp
+++ DP/304.cpp
@@ -31,3 +31,20 @@ int solver304() {
 
     return 0;
 }
+
+
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < nums.size(); ++i){
+            nums[i] += nums[i-1];
+        }
+        vector<int> ans;
+        for(auto& q : queries){
+            auto it = upper_bound(nums.begin(), nums.end(), q);
+            ans.push_back(it - nums.begin());
+        }
+        return ans;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 
 #include<bits/stdc++.h>
+
+using namespace std;
+
 int solver702();
 int main() {
     // INIT
     // test git
+    cout << "Running 702" << endl;
     solver702();
 
     return 0;
